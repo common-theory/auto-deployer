5381643c8c3af344d82d0a8a31ae49ab69f359cd
Chance Hudson <jchancehud@gmail.com>
2018-09-19 02:15:57 -0500

    Fix listen console log

--- server.ts
+++ server.ts
@@ -20,4 +20,4 @@ app.use((req: express.Request, res: express.Response, next: express.NextFunction
   res.send('Hello World');
 });
 
-app.listen(3000, () => 'auto-deployer listening on port 3000');
+app.listen(3000, () => console.log('auto-deployer listening on port 3000'));
